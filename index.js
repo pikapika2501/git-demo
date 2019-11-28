869743e87b5f475910751b24b0a845165faf6e2c
pikapika2501 <fpgaetos@gmail.com>
2019-11-29 02:00:43 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express'); //common js modules
+//import express from 'express'; //es2015 modules
+
+const app = express();
+
+console.log("starting app");
+
+app.get(
+    '/',
+    (req, res) => {
+        res.send(
+            {hi:'there'}
+        );
+
+        console.log("inside res/rep");
+    }
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
